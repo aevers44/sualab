fba7754f9be4b4380a7e60c25e4c94cb791598c3
woowong <noradvii@gmail.com>
2018-01-28 01:29:08 +0900

    Add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import MainPage from './mainPage';
 import Test from './test';
