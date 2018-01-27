b884d9681a591fc2c6e952a77bafce9579b89951
woowong <noradvii@gmail.com>
2018-01-28 02:05:02 +0900

    Make header

--- src/components/commons/header/index.jsx
+++ src/components/commons/header/index.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './header.scss';
 
 const Header = () => 
-    <header>
-        header context
+    <header className={styles.header}>
+        <div className={styles.innerContainer}>
+            <div className={styles.logoImage}>
+                <Link to="/">
+                    <img src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/sualab-logo@2x.png" alt=""/>
+                </Link>
+            </div>
+
+            <div className={styles.menuWrapper}>
+                <Link className={styles.menuItem} to="/">Company</Link>
+                <Link className={styles.menuItem} to="/">Product</Link>
+                <Link className={styles.menuItem} to="/">News</Link>
+                <Link className={styles.menuItem} to="/">Career</Link>
+                <Link className={styles.menuItem} to="/">Blog</Link>
+                <Link className={styles.menuItem} to="/">Contact</Link>
+            </div>
+        </div>
     </header>;
 
 export default Header;
