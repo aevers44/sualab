e4decb49af2cc2540c1aa598bf1f2b215ccedd32
woowong <noradvii@gmail.com>
2018-01-28 23:01:22 +0900

    make header underbar when menu enter

--- src/components/commons/header/index.jsx
+++ src/components/commons/header/index.jsx
@@ -4,26 +4,61 @@ import { Link, Route } from 'react-router-dom';
 import Navbar from './navbar';
 import styles from './header.scss';
 
-const Header = () => 
-    <header className={styles.header}>
-        <div className={styles.innerContainer}>
-            <div className={styles.logoImage}>
-                <Link to="/">
-                    <img src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/sualab-logo@2x.png" alt=""/>
-                </Link>
+const Header = () => {
+    const curPath = window.location.pathname.split('/')[1];
+    
+    return (
+        <header className={styles.header}>
+            <div className={styles.innerContainer}>
+                <div className={styles.logoImage}>
+                    <Link to="/">
+                        <img src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/sualab-logo@2x.png" alt=""/>
+                    </Link>
+                </div>
+
+                <div className={styles.menuWrapper}>
+                    <Link 
+                        className={`${styles.menuItem} ${curPath === 'company' ? styles.active : ''}`} 
+                        to="/company"
+                    >
+                        Company
+                    </Link>
+                    <Link 
+                        className={`${styles.menuItem} ${curPath === 'product' ? styles.active : ''}`} 
+                        to="/"
+                    >
+                        Product
+                    </Link>
+                    <Link 
+                        className={`${styles.menuItem} ${curPath === 'news' ? styles.active : ''}`} 
+                        to="/"
+                    >
+                        News
+                    </Link>
+                    <Link 
+                        className={`${styles.menuItem} ${curPath === 'career' ? styles.active : ''}`} 
+                        to="/"
+                    >
+                        Career
+                    </Link>
+                    <Link 
+                        className={`${styles.menuItem} ${curPath === 'blog' ? styles.active : ''}`} 
+                        to="/"
+                    >
+                        Blog
+                    </Link>
+                    <Link 
+                        className={`${styles.menuItem} ${curPath === 'contact' ? styles.active : ''}`} 
+                        to="/"
+                    >
+                        Contact
+                    </Link>
+                </div>
             </div>
 
-            <div className={styles.menuWrapper}>
-                <Link className={styles.menuItem} to="/company">Company</Link>
-                <Link className={styles.menuItem} to="/">Product</Link>
-                <Link className={styles.menuItem} to="/">News</Link>
-                <Link className={styles.menuItem} to="/">Career</Link>
-                <Link className={styles.menuItem} to="/">Blog</Link>
-                <Link className={styles.menuItem} to="/">Contact</Link>
-            </div>
-        </div>
-
-        <Route path="/company" component={Navbar} />
-    </header>;
+            <Route path="/:page/:subpage" component={Navbar} />
+        </header>
+    );
+}
 
 export default Header;
--- src/components/commons/header/navbar/index.jsx
+++ src/components/commons/header/navbar/index.jsx
@@ -13,28 +13,35 @@ const linkItem = {
     ]
 };
 
-const makeLinks = (path) => {
+const makeLinks = (curPath, subPath) => {
     let result = [];
 
-    for (let idx in linkItem[path]) {
-        const item = linkItem[path][idx];
-        
-        result.push(<Link key={item.link} className={styles.linkItem} to={item.link}>{item.name}</Link>);
+    for (let idx in linkItem[curPath]) {
+        const item = linkItem[curPath][idx];
+
+        result.push(
+            <Link 
+                key={item.link} 
+                className={`${styles.linkItem} ${subPath === item.link ? styles.active : ''}`}
+                to={item.link}
+            >{item.name}</Link>
+        );
     }
 
     return result;
 }
 
 const Navbar = ({ match }) => {
-    const currentPath = match.path.split('/')[1];
-    const linkItems = makeLinks(currentPath);
+    const curPath = match.params.page;
+    const subPath = match.params.subpage;
+    const linkItems = makeLinks(curPath, subPath);
 
     return (
         <nav className={styles.navbar}>
             <div className={styles.innerContainer}>
                 <div 
                     className={styles.linkWrapper} 
-                    style={{marginRight: `${marginList[currentPath]}px`}}
+                    style={{marginRight: `${marginList[curPath]}px`}}
                 >
                     {linkItems}         
                 </div>
