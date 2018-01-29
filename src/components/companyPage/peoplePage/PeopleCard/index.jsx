7e7f9a1831efa087101002896201dd1576b90d68
woowong <noradvii@gmail.com>
2018-01-30 00:55:24 +0900

    finish peoplePage

--- src/components/companyPage/peoplePage/PeopleCard/index.jsx
+++ src/components/companyPage/peoplePage/PeopleCard/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-const PeopleCard = () => 
-    <div>
-        card
+import styles from './peopleCard.scss';
+
+const PeopleCard = ({ imgUrl, position, name, engName, detail }) => 
+    <div className={styles.peopleCard}>
+        <div className={styles.innerWrapper}>
+            <div className={styles.imageWrapper}>
+                <img src={imgUrl} alt=""/>
+            </div>
+            <p className={styles.position}>{position}</p>
+            <p className={styles.name}>{name}</p>
+            <p className={styles.engName}>{engName}</p>
+        </div>
+
+        <div className={styles.detailWrapper}>
+            <div className={styles.detailContent}>
+                {detail.join('\n')}
+            </div>
+        </div>
     </div>;
 
 export default PeopleCard;
