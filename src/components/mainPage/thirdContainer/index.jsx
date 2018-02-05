44bd5bc878cdfc71b54c97776aa5ae921fd2f8b9
kimyukyeong <kykkjh2984@gmail.com>
2018-02-06 01:17:43 +0900

    Change home background img and edit button size

--- src/components/mainPage/thirdContainer/index.jsx
+++ src/components/mainPage/thirdContainer/index.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import styles from './thirdContainer.scss';
+import React from "react";
+import styles from "./thirdContainer.scss";
 
-const ThirdContainer = () =>
-    <section className={styles.thirdContainer}>
-        <div className={styles.innerContainer}>
-            <div className={styles.contentWrapper}>
-                <div className={styles.title}>
-                    CUSTOMER
-                </div>
-                <div className={styles.bigTitle}>
-                    고객사
-                </div>
-                <div className={styles.content}>
-                    SUALAB은 유수의 대기업들로 부터 <br />
-                    훌륭한 기술력을 입증 받아 다양한 솔루션을 납품해 왔습니다.
-                </div>
-
-                <a href="#" className={styles.readmoreBtn}>
-                    Read More
-                    <img src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/combined-shape@2x.png" alt=""/>
-                </a>
-            </div>
-
-            <div className={styles.imageWrapper}>
-                <img src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/customers@2x.png" alt="" className={styles.customers}/>
-            </div>
+const ThirdContainer = () => (
+  <section className={styles.thirdContainer}>
+    <div className={styles.innerContainer}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.title}>CUSTOMER</div>
+        <div className={styles.bigTitle}>고객사</div>
+        <div className={styles.content}>
+          SUALAB은 유수의 대기업들로 부터 <br />
+          훌륭한 기술력을 입증 받아 다양한 솔루션을 납품해 왔습니다.
         </div>
-    </section>
+
+        <a href="#" className={styles.readmoreBtn}>
+          Read More
+          <img src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/combined-shape@2x.png" alt="" />
+        </a>
+      </div>
+
+      <div className={styles.imageWrapper}>
+        <img
+          src="https://d3rqapinkc8b8p.cloudfront.net/mainPage/customers%402x.png"
+          alt=""
+          className={styles.customers}
+        />
+      </div>
+    </div>
+  </section>
+);
 
 export default ThirdContainer;
-
