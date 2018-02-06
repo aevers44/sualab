633ad94830229ad4255bdea22aaf1eed13662450
woowong <noradvii@gmail.com>
2018-02-07 00:21:48 +0900

    make career/recruit page

--- src/components/careerPage/recruitPage/index.jsx
+++ src/components/careerPage/recruitPage/index.jsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React from "react";
 
-import styles from './recruitPage.scss';
+import TitleSection from "./titleSection";
 
-const RecruitPage = () => 
-    <section className={styles.recruitPage}>
-        hello recruit RecruitPage
-    </section>;
+import styles from "./recruitPage.scss";
 
+const RecruitPage = () => (
+  <section className={styles.recruitPage}>
+    <TitleSection />
+    <div className={styles.innerContainer}>
+      <div className={styles.line} />
+      <div className={styles.subTitle}>연구소</div>
+      <div className={styles.rndGroupWrapper}>
+        <div className={styles.infoTextBox1}>
+          연구 인력 채용은 각 직무별로 상이 할 수 있으니, <br />
+          자세한 내용은 <span className={styles.highlight}>채용공고</span>를 통해 확인 바랍니다.
+        </div>
+
+        <div className={styles.stepCircleWrapper}>
+          <div className={`${styles.stepCircle} ${styles.step1}`}>
+            <span className={styles.step}>STEP 01</span>
+            <span className={styles.text}>서류 심사</span>
+          </div>
+          <div className={`${styles.stepCircle} ${styles.step2}`}>
+            <span className={styles.step}>STEP 02</span>
+            <span className={styles.text}>전화 면접</span>
+          </div>
+          <div className={`${styles.stepCircle} ${styles.step3}`}>
+            <span className={styles.step}>STEP 03</span>
+            <span className={styles.text}>1차 면접</span>
+          </div>
+          <div className={`${styles.stepCircle} ${styles.step4}`}>
+            <span className={styles.step}>STEP 04</span>
+            <span className={styles.text}>프로젝트 과제</span>
+
+            <div className={styles.infoTextBox2}>
+              프로젝트 과제는 직무의 필요한 역량을 검증하기 위한 단계이며, <br />
+              평가에 따라 2차 면접 진행 여부가 결정됩니다. <br />
+              <span className={styles.red}>*해당 전형은 일부 직무에서만 진행됩니다.</span>
+            </div>
+          </div>
+          <div className={`${styles.stepCircle} ${styles.step5}`}>
+            <span className={styles.step}>STEP 05</span>
+            <span className={styles.text}>2차 면접</span>
+          </div>
+          <div className={`${styles.stepCircle} ${styles.step6}`}>
+            <span className={styles.step}>STEP 06</span>
+            <span className={styles.text}>채용 확정</span>
+          </div>
+
+          <div className={styles.bgLine} />
+        </div>
+      </div>
+
+      <div className={styles.line} />
+      <div className={styles.subTitle}>영업본부 및 경영관리본부</div>
+
+      <div className={styles.stepCircleWrapper}>
+        <div className={`${styles.stepCircle} ${styles.step3}`}>
+          <span className={styles.step}>STEP 01</span>
+          <span className={styles.text}>서류 심사</span>
+        </div>
+        <div className={`${styles.stepCircle} ${styles.step4}`}>
+          <span className={styles.step}>STEP 02</span>
+          <span className={styles.text}>1차 면접</span>
+        </div>
+        <div className={`${styles.stepCircle} ${styles.step5}`}>
+          <span className={styles.step}>STEP 03</span>
+          <span className={styles.text}>2차 면접</span>
+        </div>
+        <div className={`${styles.stepCircle} ${styles.step6}`}>
+          <span className={styles.step}>STEP 04</span>
+          <span className={styles.text}>채용 확정</span>
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default RecruitPage;
--- src/components/commons/header/navbar/index.jsx
+++ src/components/commons/header/navbar/index.jsx
@@ -1,61 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './navbar.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./navbar.scss";
 
 const marginList = {
-    'company': 275,
-    'news': 160,
+  company: 275,
+  news: 160,
+  career: 120,
 };
 
 const linkItem = {
-    'company': [
-        {'name': 'About us', 'link': 'about-us'},
-        {'name': 'People', 'link': 'people'}
-    ],
-    'news': [
-        {'name': 'SUALAB in media', 'link': 'media'},
-    ],
-    'carrer': [
-        {'name': '채용 절차', 'link': 'recruit'},
-    ],
+  company: [{ name: "About us", link: "about-us" }, { name: "People", link: "people" }],
+  news: [{ name: "SUALAB in media", link: "media" }],
+  career: [{ name: "채용 절차", link: "recruit" }],
 };
 
 const makeLinks = (curPath, subPath) => {
-    let result = [];
+  let result = [];
 
-    for (let idx in linkItem[curPath]) {
-        const item = linkItem[curPath][idx];
+  for (let idx in linkItem[curPath]) {
+    const item = linkItem[curPath][idx];
 
-        result.push(
-            <Link 
-                key={item.link} 
-                className={`${styles.linkItem} ${subPath === item.link ? styles.active : ''}`}
-                to={item.link}
-            >{item.name}</Link>
-        );
-    }
+    result.push(
+      <Link
+        key={item.link}
+        className={`${styles.linkItem} ${subPath === item.link ? styles.active : ""}`}
+        to={item.link}
+      >
+        {item.name}
+      </Link>,
+    );
+  }
 
-    return result;
-}
+  return result;
+};
 
 const Navbar = ({ match }) => {
-    const curPath = match.params.page;
-    const subPath = match.params.subpage;
-    const linkItems = makeLinks(curPath, subPath);
+  const curPath = match.params.page;
+  const subPath = match.params.subpage;
+  const linkItems = makeLinks(curPath, subPath);
 
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.innerContainer}>
-                <div 
-                    className={styles.linkWrapper} 
-                    style={{marginRight: `${marginList[curPath]}px`}}
-                >
-                    {linkItems}         
-                </div>
-            </div>
-        </nav>
-    );
-
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.innerContainer}>
+        <div className={styles.linkWrapper} style={{ marginRight: `${marginList[curPath]}px` }}>
+          {linkItems}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
