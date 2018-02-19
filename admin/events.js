4ce6ca857ff92c9a4f48db4227113494c89376b5
woowong <noradvii@gmail.com>
2018-02-19 22:14:37 +0900

    Make image upload

--- admin/events.js
+++ admin/events.js
@@ -1,4 +1,49 @@
-exports.postSave = function(req, res, args, next) {
-  console.log("postSave");
-  next();
+var AWS = require("aws-sdk");
+var path = require("path");
+var fs = require("fs");
+
+AWS.config.region = "ap-northeast-2";
+AWS.config.update({
+  accessKeyId: "AKIAJIOJ4D5IJ5AAANLA",
+  secretAccessKey: "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",
+});
+
+exports.preSave = function(req, res, args, next) {
+  if (args.name == "media" || args.name == "event") {
+    var image = args.upload.view[args.name].records[0].columns.image;
+
+    if (image.name) {
+      var fname = args.data.view[args.name].records[0].columns.image;
+      var record = args.data.view[args.name].records[0].columns;
+
+      var date = new Date();
+      var datePath = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "/";
+
+      var s3_params = {
+        Bucket: "sualab-assets",
+        Key: "upload/" + datePath + image.name,
+        ACL: "public-read",
+        ContentType: image.type,
+      };
+
+      var s3 = new AWS.S3({ params: s3_params });
+
+      s3
+        .upload({ Body: fs.createReadStream(image.path) })
+        .on("httpUploadProgress", function(evt) {
+          console.log(evt);
+        })
+        .send(function(err, data) {
+          //S3 File URL
+          var url = data.Location;
+          console.log(url);
+          record.image = url;
+          next();
+        });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 };
