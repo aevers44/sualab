55c19e4d59ad079b8bebab9cb6170fb45c8c5100
woowong <noradvii@gmail.com>
2018-02-19 16:52:21 +0900

    make secondsecition

--- src/components/careerPage/philosophyPage/firstSection/index.jsx
+++ src/components/careerPage/philosophyPage/firstSection/index.jsx
@@ -18,18 +18,18 @@ const FirstSection = () => (
         </div>
 
         <div className={styles.cardItem}>
-          <div className={styles.cardTitle}>최고의 인재</div>
+          <div className={styles.cardTitle}>정당한 보상</div>
           <div className={styles.cardContent}>
-            최고의 동료와 함께 <br />
-            일하는 것이 최고의 복지
+            구성원들의 노력에 대한 <br />
+            정당한 보상을 제공
           </div>
         </div>
 
         <div className={styles.cardItem}>
-          <div className={styles.cardTitle}>최고의 인재</div>
+          <div className={styles.cardTitle}>즐겁게 일할 수 있는 환경</div>
           <div className={styles.cardContent}>
-            최고의 동료와 함께 <br />
-            일하는 것이 최고의 복지
+            즐겁게 일할 수 있는 <br />
+            문화와 환경을 조성하겠다는 철학
           </div>
         </div>
       </div>
