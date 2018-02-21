4b21fbc0a7b51db65a948423b83d2d4e270de958
woowong <noradvii@gmail.com>
2018-02-21 18:24:21 +0900

    fix failure

--- src/components/mainPage/firstContainer/index.jsx
+++ src/components/mainPage/firstContainer/index.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< Updated upstream
-=======
 import { Link } from "react-router-dom";
->>>>>>> Stashed changes
 import styles from "./firstContainer.scss";
 
 const FirstContainer = () => (
