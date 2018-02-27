87590bf692c170f7ffa6a8610c650f0424397892
woowong <noradvii@gmail.com>
2018-02-28 02:59:01 +0900

    Extract messages from aboutPage

--- src/components/commons/intl/ko.js
+++ src/components/commons/intl/ko.js
@@ -10,4 +10,14 @@ export default {
   "MAIN.FOURTH.subTitle": `딥러닝 머신비전, 머신 러닝 등 SUALAB의 기술에 대해 더 자세히 알고 싶다면?`,
   "MAIN.FOURTH.blogBtn": `SUALAB 기술 블로그`,
   "MAIN.FIFTH.bigTitle": `최신 소식`,
+
+  "ABOUT.FIRST.title": `SUALAB은 *머신비전 분야*에 특화된 *딥러닝 검사 솔루션*을 제공하는 글로벌 기업입니다`,
+  "ABOUT.SECOND.missionSubContent": `노력에 대한 정당한 보상과 일하는 즐거움을 통해 구성원들을 행복하게 하고,\n사람이 기계 대신 하던 일에서 벗어나도록 하여 세상에 기여합니다.`,
+  "ABOUT.SECOND.visionSubContent": `딥러닝 기반 Vision Inspection 분야의 Global No.1`,
+  "ABOUT.THIRD.title": `*딥러닝 머신비전 솔루션* 글로벌 리더`,
+  "ABOUT.THIRD.content": `SUALAB은 인공지능을 기반으로 한 Deep Learning Machine Vison Solution Provider 입니다.\n\nSUALAB은 컴퓨터, 전자, 기계 분야 등의 우수한 엔지니어들이 모여 각 분야의 기술을 융합하여 인간을 보다 편리하게 하는 다양한 기술과 제품을 연구합니다.\n\n특히, 딥러닝, 머신비전 그리고 슈퍼컴퓨팅 기술을 바탕으로 산업현장에서 인간의 눈을 대신 할 수 있는 다양한 머신 비전 시스템을 연구하여 제조 산업의 정밀 검사를 가능하게 합니다.`,
+  "ABOUT.FOURTH.subContent": `SUALAB은 인공지능 이미지 해석 기술을 기반으로, 기존 머신비전 기술로 검사가 어려운\n디스플레이, 태양광, PCB, 필름, 반도체 등의 분야를 위한 무인 검사 자동화 솔루션을 제공합니다.`,
+  "ABOUT.FOURTH.itemContent_1": `딥러닝 학습 최적화를 위해 머신비전 알고리즘 등 다양한 기술을 활용하여 이미지 전처리를 수행합니다.`,
+  "ABOUT.FOURTH.itemContent_2": `SUALAB이 자체 개발한 학습망과 GPU 컴퓨팅 기술을 활용하여 1000분의 1초 단위의 실시간 분석을 진행합니다.`,
+  "ABOUT.FOURTH.itemContent_3": `검사 리포트 생성, 검사 결과 Database화, 불량 지점 마킹 등이 가능한 S/W를 제공합니다.`,
 };
