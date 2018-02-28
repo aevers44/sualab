da86b17c1508dc7ed06ecfb655694897465d3393
woowong <noradvii@gmail.com>
2018-02-28 15:11:18 +0900

    extract from howtopage

--- src/components/productPage/howToPage/index.jsx
+++ src/components/productPage/howToPage/index.jsx
@@ -11,9 +11,8 @@ const HowToPage = ({ intl }) => (
   <section className={styles.howToPage}>
     <TitleSection
       subTitle="HOW TO APPLY"
-      title="*누구나* 손쉽게 *딥러닝 모델을* 생성하고 *제조 현장에 적용*할 수 있습니다"
+      title={intl.formatMessage({ id: "HOWTO.title" })}
       bgImage="https://d2ivzy5c3eic08.cloudfront.net/productPage/sualab-product-howtoapply-background@2x.jpg"
-      intl={intl}
     />
 
     <UsageSection intl={intl} />
