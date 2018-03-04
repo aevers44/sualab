c8168300a93f764aef96ff22a42970b8f91f5bf3
woowong <noradvii@gmail.com>
2018-03-04 18:01:05 +0900

    Change peopledata for using DB

--- src/components/companyPage/peoplePage/PeopleCard/index.jsx
+++ src/components/companyPage/peoplePage/PeopleCard/index.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import styles from './peopleCard.scss';
+import styles from "./peopleCard.scss";
 
-const PeopleCard = ({ imgUrl, position, name, engName, detail }) => 
-    <div className={styles.peopleCard}>
-        <div className={styles.innerWrapper}>
-            <div className={styles.imageWrapper}>
-                <img src={imgUrl} alt=""/>
-            </div>
-            <p className={styles.position}>{position}</p>
-            <p className={styles.name}>{name}</p>
-            <p className={styles.engName}>{engName}</p>
-        </div>
+const PeopleCard = ({ locale, imgUrl, position, name, eng_name, detail, eng_detail }) => (
+  <div className={styles.peopleCard}>
+    <div className={styles.innerWrapper}>
+      <div className={styles.imageWrapper}>
+        <img src={imgUrl} alt="" />
+      </div>
+      <p className={styles.position}>{position}</p>
+      <p className={styles.name}>{locale === "ko" ? name : eng_name}</p>
+      <p className={styles.engName}>{locale === "ko" ? eng_name : ""}</p>
+    </div>
 
-        <div className={styles.detailWrapper}>
-            <div className={styles.detailContent}>
-                {detail.join('\n')}
-            </div>
-        </div>
-    </div>;
+    <div className={styles.detailWrapper}>
+      <div className={styles.detailContent}>{locale === "ko" ? detail : eng_detail}</div>
+    </div>
+  </div>
+);
 
 export default PeopleCard;
