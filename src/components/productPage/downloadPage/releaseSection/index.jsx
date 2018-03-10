af91aec0270e02ca54c90a7d5f80c44afd095df3
woowong <noradvii@gmail.com>
2018-03-10 15:53:51 +0900

    make release sefction

--- src/components/productPage/downloadPage/releaseSection/index.jsx
+++ src/components/productPage/downloadPage/releaseSection/index.jsx
@@ -5,7 +5,39 @@ import styles from "./releaseSection.scss";
 class ReleaseSection extends React.PureComponent {
   render() {
     const { intl } = this.props;
-    return <section className={styles.releaseSection}>releaseSection</section>;
+    return (
+      <section className={styles.releaseSection}>
+        <div className={styles.innerContainer}>
+          <div className={styles.line} />
+          <div className={styles.title}>Release Note</div>
+
+          <div className={styles.releaseTable}>
+            <div className={styles.tableTitle}>Basics</div>
+            <div className={styles.tableItem}>
+              <span className={styles.version}>v2.0.2</span>
+              <span className={styles.date}>01/Mar/2018</span>
+              <a href="#" className={styles.content}>
+                상세 내용
+              </a>
+            </div>
+            <div className={styles.tableItem}>
+              <span className={styles.version}>v2.0.2</span>
+              <span className={styles.date}>01/Mar/2018</span>
+              <a href="#" className={styles.content}>
+                상세 내용
+              </a>
+            </div>
+            <div className={styles.tableItem}>
+              <span className={styles.version}>v2.0.2</span>
+              <span className={styles.date}>01/Mar/2018</span>
+              <a href="#" className={styles.content}>
+                상세 내용
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
