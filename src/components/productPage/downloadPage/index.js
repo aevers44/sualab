5ed8a70c81969fc2d16dd492a81fd96055709625
woowong <noradvii@gmail.com>
2018-03-16 17:55:07 +0900

    download page title iamge

--- src/components/productPage/downloadPage/index.js
+++ src/components/productPage/downloadPage/index.js
@@ -14,7 +14,11 @@ class DownloadPage extends React.PureComponent {
     const { intl } = this.props;
     return (
       <section className={styles.downloadPage}>
-        <TitleSection subTitle="DOWNLOAD" title={intl.formatMessage({ id: "DOWNLOAD.title" })} bgImage="" />
+        <TitleSection
+          subTitle="DOWNLOAD"
+          title={intl.formatMessage({ id: "DOWNLOAD.title" })}
+          bgImage="https://d2ivzy5c3eic08.cloudfront.net/productPage/product-download%402x.png"
+        />
 
         <SuakitSection intl={intl} />
         <FeatureSection intl={intl} />
