8a9e6bb4e495bdeb85ed802fc604a9cc6d221e50
woowong <noradvii@gmail.com>
2018-06-18 01:25:39 +0900

    carrer/recruit process

--- src/components/careerPage/recruitPage/index.jsx
+++ src/components/careerPage/recruitPage/index.jsx
@@ -17,11 +17,17 @@ const RecruitPage = ({ intl }) => {
       />
       <div className={styles.innerContainer}>
         <div className={styles.line} />
-        <div className={styles.subTitle}>{intl.formatMessage({ id: "RECRUIT.LAB.subTitle" })}</div>
+        <div className={styles.subTitle}>
+          {intl.formatMessage({ id: "RECRUIT.LAB.subTitle" })}
+        </div>
         <div className={styles.rndGroupWrapper}>
           <div
             className={styles.infoTextBox1}
-            dangerouslySetInnerHTML={{ __html: replaceSpan(intl.formatMessage({ id: "RECRUIT.LAB.infoTextBox1" })) }}
+            dangerouslySetInnerHTML={{
+              __html: replaceSpan(
+                intl.formatMessage({ id: "RECRUIT.LAB.infoTextBox1" }),
+              ),
+            }}
           />
 
           <div className={styles.stepCircleWrapper}>
@@ -29,57 +35,85 @@ const RecruitPage = ({ intl }) => {
 
             <div className={`${styles.stepCircle} ${styles.step1}`}>
               <span className={styles.step}>STEP 01</span>
-              <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.LAB.step_1" })}</span>
+              <span className={styles.text}>
+                {intl.formatMessage({ id: "RECRUIT.LAB.step_1" })}
+              </span>
             </div>
             <div className={`${styles.stepCircle} ${styles.step2}`}>
               <span className={styles.step}>STEP 02</span>
-              <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.LAB.step_2" })}</span>
+              <span className={styles.text}>
+                {intl.formatMessage({ id: "RECRUIT.LAB.step_2" })}
+              </span>
             </div>
             <div className={`${styles.stepCircle} ${styles.step3}`}>
               <span className={styles.step}>STEP 03</span>
-              <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.LAB.step_3" })}</span>
+              <span className={styles.text}>
+                {intl.formatMessage({ id: "RECRUIT.LAB.step_3" })}
+              </span>
             </div>
-            <div className={`${styles.stepCircle} ${styles.step4}`}>
+            <div
+              className={`${styles.stepCircle} ${styles.step4} ${
+                styles.mobilePadding
+              }`}
+            >
               <span className={styles.step}>STEP 04</span>
-              <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.LAB.step_4" })}</span>
+              <span className={styles.text}>
+                {intl.formatMessage({ id: "RECRUIT.LAB.step_4" })}
+              </span>
 
               <div
                 className={styles.infoTextBox2}
                 dangerouslySetInnerHTML={{
-                  __html: replaceSpan(intl.formatMessage({ id: "RECRUIT.LAB.infoTextBox2" })),
+                  __html: replaceSpan(
+                    intl.formatMessage({ id: "RECRUIT.LAB.infoTextBox2" }),
+                  ),
                 }}
               />
             </div>
             <div className={`${styles.stepCircle} ${styles.step5}`}>
               <span className={styles.step}>STEP 05</span>
-              <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.LAB.step_5" })}</span>
+              <span className={styles.text}>
+                {intl.formatMessage({ id: "RECRUIT.LAB.step_5" })}
+              </span>
             </div>
             <div className={`${styles.stepCircle} ${styles.step6}`}>
               <span className={styles.step}>STEP 06</span>
-              <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.LAB.step_6" })}</span>
+              <span className={styles.text}>
+                {intl.formatMessage({ id: "RECRUIT.LAB.step_6" })}
+              </span>
             </div>
           </div>
         </div>
 
         <div className={styles.line} />
-        <div className={styles.subTitle}>{intl.formatMessage({ id: "RECRUIT.MANAGE.subTitle" })}</div>
+        <div className={styles.subTitle}>
+          {intl.formatMessage({ id: "RECRUIT.MANAGE.subTitle" })}
+        </div>
 
         <div className={styles.stepCircleWrapper}>
           <div className={`${styles.stepCircle} ${styles.step3}`}>
             <span className={styles.step}>STEP 01</span>
-            <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.MANAGE.step_1" })}</span>
+            <span className={styles.text}>
+              {intl.formatMessage({ id: "RECRUIT.MANAGE.step_1" })}
+            </span>
           </div>
           <div className={`${styles.stepCircle} ${styles.step4}`}>
             <span className={styles.step}>STEP 02</span>
-            <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.MANAGE.step_2" })}</span>
+            <span className={styles.text}>
+              {intl.formatMessage({ id: "RECRUIT.MANAGE.step_2" })}
+            </span>
           </div>
           <div className={`${styles.stepCircle} ${styles.step5}`}>
             <span className={styles.step}>STEP 03</span>
-            <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.MANAGE.step_3" })}</span>
+            <span className={styles.text}>
+              {intl.formatMessage({ id: "RECRUIT.MANAGE.step_3" })}
+            </span>
           </div>
           <div className={`${styles.stepCircle} ${styles.step6}`}>
             <span className={styles.step}>STEP 04</span>
-            <span className={styles.text}>{intl.formatMessage({ id: "RECRUIT.MANAGE.step_4" })}</span>
+            <span className={styles.text}>
+              {intl.formatMessage({ id: "RECRUIT.MANAGE.step_4" })}
+            </span>
           </div>
         </div>
       </div>
