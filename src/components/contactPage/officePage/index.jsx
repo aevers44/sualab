88dba933f62e50671ae1fade4970d2e2bce2ef2a
woowong <noradvii@gmail.com>
2018-06-18 01:46:11 +0900

    contact/office process

--- src/components/contactPage/officePage/index.jsx
+++ src/components/contactPage/officePage/index.jsx
@@ -17,7 +17,9 @@ const OfficePage = ({ intl }) => (
       <div className={styles.officeWrapper}>
         <div className={styles.titleWrapper}>
           <div className={styles.line} />
-          <div className={styles.subTitle}>{intl.formatMessage({ id: "OFFICE.base" })}</div>
+          <div className={styles.subTitle}>
+            {intl.formatMessage({ id: "OFFICE.base" })}
+          </div>
         </div>
 
         <div className={styles.contentWrapper}>
@@ -26,36 +28,54 @@ const OfficePage = ({ intl }) => (
               isMarkerShown
               googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,placess&key=AIzaSyC2l8AiJrv0oRBAQIKI60rgH16h93W98Ac"
               loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `450px` }} />}
+              containerElement={<div className={styles.mapContainer} />}
               mapElement={<div style={{ height: `100%` }} />}
             />
           </div>
 
           <div className={styles.infoLine}>
-            <div className={styles.label}>{intl.formatMessage({ id: "OFFICE.address" })}</div>
-            <div className={styles.info}>{intl.formatMessage({ id: "OFFICE.base.address" })}</div>
-          </div>
-
-          <div className={styles.infoLine}>
-            <div className={styles.label}>{intl.formatMessage({ id: "OFFICE.phone" })}</div>
+            <div className={styles.label}>
+              {intl.formatMessage({ id: "OFFICE.address" })}
+            </div>
             <div className={styles.info}>
-              <span>{intl.formatMessage({ id: "OFFICE.question.common" })} | </span>+82 2-6264-0366<br />
-              <span>{intl.formatMessage({ id: "OFFICE.question.sales" })} | </span>+82 2-6264-0362
+              {intl.formatMessage({ id: "OFFICE.base.address" })}
             </div>
           </div>
 
           <div className={styles.infoLine}>
-            <div className={styles.label}>{intl.formatMessage({ id: "OFFICE.fax" })}</div>
+            <div className={styles.label}>
+              {intl.formatMessage({ id: "OFFICE.phone" })}
+            </div>
+            <div className={styles.info}>
+              <span>
+                {intl.formatMessage({ id: "OFFICE.question.common" })} |{" "}
+              </span>+82 2-6264-0366<br />
+              <span>
+                {intl.formatMessage({ id: "OFFICE.question.sales" })} |{" "}
+              </span>+82 2-6264-0362
+            </div>
+          </div>
+
+          <div className={styles.infoLine}>
+            <div className={styles.label}>
+              {intl.formatMessage({ id: "OFFICE.fax" })}
+            </div>
             <div className={styles.info}>+82 2-6264-0363</div>
           </div>
 
           <div className={styles.infoLine}>
-            <div className={styles.label}>{intl.formatMessage({ id: "OFFICE.email" })}</div>
+            <div className={styles.label}>
+              {intl.formatMessage({ id: "OFFICE.email" })}
+            </div>
             <div className={styles.info}>
-              <span>{intl.formatMessage({ id: "OFFICE.question.recruit" })} | </span>
+              <span>
+                {intl.formatMessage({ id: "OFFICE.question.recruit" })} |{" "}
+              </span>
               <a href="mailto:recruit@sualab.com">recruit@sualab.com</a>
               <br />
-              <span>{intl.formatMessage({ id: "OFFICE.question.sales" })} | </span>
+              <span>
+                {intl.formatMessage({ id: "OFFICE.question.sales" })} |{" "}
+              </span>
               <a href="mailto:sales@sualab.com">sales@sualab.com</a>
             </div>
           </div>
@@ -65,21 +85,30 @@ const OfficePage = ({ intl }) => (
       <div className={styles.officeWrapper}>
         <div className={styles.titleWrapper}>
           <div className={styles.line} />
-          <div className={styles.subTitle}>{intl.formatMessage({ id: "OFFICE.china" })}</div>
+          <div className={styles.subTitle}>
+            {intl.formatMessage({ id: "OFFICE.china" })}
+          </div>
         </div>
 
         <div className={`${styles.contentWrapper} ${styles.blankBox}`}>
           <div className={styles.infoLine}>
-            <div className={styles.label}>{intl.formatMessage({ id: "OFFICE.address" })}</div>
+            <div className={styles.label}>
+              {intl.formatMessage({ id: "OFFICE.address" })}
+            </div>
             <div className={styles.info}>
-              4-B801, Creative Industrial Park, 328 Xinghu Street, Suzhou Industrial Park
+              4-B801, Creative Industrial Park, 328 Xinghu Street, Suzhou
+              Industrial Park
             </div>
           </div>
 
           <div className={styles.infoLine}>
-            <div className={styles.label}>{intl.formatMessage({ id: "OFFICE.phone" })}</div>
+            <div className={styles.label}>
+              {intl.formatMessage({ id: "OFFICE.phone" })}
+            </div>
             <div className={styles.info}>
-              <span>{intl.formatMessage({ id: "OFFICE.phone.represent" })} | </span>+86 0512 62650852
+              <span>
+                {intl.formatMessage({ id: "OFFICE.phone.represent" })} |{" "}
+              </span>+86 0512 62650852
             </div>
           </div>
         </div>
