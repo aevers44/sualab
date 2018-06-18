613280cb9e160abfd47348fc6e44345556f77f0b
woowong <noradvii@gmail.com>
2018-06-18 12:26:18 +0900

    change image of about/third

--- src/components/companyPage/aboutUsPage/thirdSection/index.jsx
+++ src/components/companyPage/aboutUsPage/thirdSection/index.jsx
@@ -12,16 +12,22 @@ const ThirdSection = ({ intl }) => {
       <div className={styles.innerContainer}>
         <div className={styles.leftWrapper}>
           <div className={styles.line} />
-          <div className={styles.title} dangerouslySetInnerHTML={{ __html: replacedTitle }} />
-          <div className={styles.content}>{intl.formatMessage({ id: "ABOUT.THIRD.content" })}</div>
+          <div
+            className={styles.title}
+            dangerouslySetInnerHTML={{ __html: replacedTitle }}
+          />
+          <div className={styles.content}>
+            {intl.formatMessage({ id: "ABOUT.THIRD.content" })}
+          </div>
         </div>
 
         <div className={styles.rightWrapper}>
-          <div className={styles.imageWrapper} />
-
-          <span className={`${styles.itemName} ${styles.first}`}>Machine Vision</span>
-          <span className={`${styles.itemName} ${styles.second}`}>Deep Learning</span>
-          <span className={`${styles.itemName} ${styles.third}`}>Super Computing</span>
+          <div className={styles.imageWrapper}>
+            <img
+              src="https://d2ivzy5c3eic08.cloudfront.net/companyPage/about-us@2x.png"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </section>
