5d74f1f7878832e63842874ac9ba2706de2c3a20
woowong <noradvii@gmail.com>
2018-06-21 19:36:04 +0900

    fix map error

--- src/components/contactPage/networkPage/googleMap.jsx
+++ src/components/contactPage/networkPage/googleMap.jsx
@@ -6,28 +6,25 @@ import {
   Marker,
 } from "react-google-maps";
 
-const NetworkMapComponent = withScriptjs(
-  withGoogleMap(props => {
-    const { markerList } = props;
-    const centerLatitude =
-      markerList.reduce((prev, curr) => prev + curr.lat, 0) / markerList.length;
-    const centerLongitude =
-      markerList.reduce((prev, curr) => prev + curr.lng, 0) / markerList.length;
-
-    return (
-      <GoogleMap
-        defaultZoom={2}
-        defaultCenter={{
-          lat: centerLatitude,
-          lng: centerLongitude,
-        }}
-      >
-        {markerList.map((elem, idx) => (
-          <Marker key={idx} position={{ ...elem }} />
-        ))}
-      </GoogleMap>
-    );
-  }),
-);
-
+const NetworkMapComponent = withGoogleMap(props => {
+  const { markerList } = props;
+  const centerLatitude =
+    markerList.reduce((prev, curr) => prev + curr.lat, 0) / markerList.length;
+  const centerLongitude =
+    markerList.reduce((prev, curr) => prev + curr.lng, 0) / markerList.length;
+  console.log(centerLatitude, centerLongitude);
+  return (
+    <GoogleMap
+      defaultZoom={2}
+      defaultCenter={{
+        lat: centerLatitude || 16.311724714285713,
+        lng: centerLongitude || 45.87756114285714,
+      }}
+    >
+      {markerList.map((elem, idx) => (
+        <Marker key={idx} position={{ ...elem }} />
+      ))}
+    </GoogleMap>
+  );
+});
 export default NetworkMapComponent;
