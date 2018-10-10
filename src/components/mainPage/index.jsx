d1e709bf995b5f3273dded8567b3d3ce066e097e
Ryan <mith1004@gmail.com>
2018-10-10 15:48:22 +0900

    add popup

--- src/components/mainPage/index.jsx
+++ src/components/mainPage/index.jsx
@@ -7,14 +7,63 @@ import ThirdContainer from "./thirdContainer";
 import FourthContainer from "./fourthContainer";
 import FifthContainer from "./fifthContainer";
 
-const MainPage = ({ intl }) => (
-  <div>
-    <FirstContainer intl={intl} />
-    <SecondContainer intl={intl} />
-    <ThirdContainer intl={intl} />
-    <FourthContainer intl={intl} />
-    <FifthContainer intl={intl} />
-  </div>
-);
+import Popup from "../popup/popup";
+
+
+const dummyPopup = [
+  {
+    id: 1,
+    image: "http://www.freedigitalphotos.net/blog/wp-content/uploads/2013/11/archie.jpg",
+    width: 300,
+    height: 400,
+    url: "http://www.naver.com"
+  },
+  {
+    id: 2,
+    image: "http://www.freedigitalphotos.net/blog/wp-content/uploads/2013/11/archie.jpg",
+    width: 600,
+    height: 200,
+    url: "http://www.naver.com"
+  }
+]
+class MainPage extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      popups: []
+    }
+  }
+  
+  componentWillMount() {
+    this.getPopup();
+  }
+
+  getPopup(){
+    this.setState({
+      popups: dummyPopup
+    })
+  }
+
+  renderPopup() {
+    return this.state.popups.map((popup,idx) => 
+    <Popup key={idx} popupId={popup.id} image={popup.image} width={popup.width} height={popup.height} url={popup.url}/>)
+  }
+
+  render () {
+    const {intl} = this.props;
+    return (
+      <div>
+        {
+          this.renderPopup()
+        }
+        <FirstContainer intl={intl} />
+        <SecondContainer intl={intl} />
+        <ThirdContainer intl={intl} />
+        <FourthContainer intl={intl} />
+        <FifthContainer intl={intl} />
+      </div>
+    )
+  }
+}
 
 export default injectIntl(MainPage);
