a3288b9523351098389fd4c6e620ce707282bdbc
wintree86 <mith1004@gmail.com>
2018-10-18 22:00:34 +0900

    update company address update product layout

--- src/components/productPage/suakitPage/firstSection/index.jsx
+++ src/components/productPage/suakitPage/firstSection/index.jsx
@@ -14,6 +14,42 @@ const FirstSection = ({ intl }) => (
         <div className={styles.content}>{intl.formatMessage({ id: "SUAKIT.FIRST.content" })}</div>
       </div>
 
+      <div className={styles.subTitle}>{intl.formatMessage({ id: "SUAKIT.FIRST.subTitle_2" })}</div>
+      <div className={styles.itemWrapper}>
+        <div className={styles.itemCard}>
+          <div className={styles.svgWrapper}>
+            <Icon icon="SUAKIT_FEATURE_2" />
+          </div>
+          <div className={styles.cardTitle}>Segmentation</div>
+          <div className={styles.cardContent}>
+            {intl.formatMessage({ id: "SUAKIT.FIRST.cardContent_1" })}
+            <p className={styles.useCase}>{intl.formatMessage({ id: "SUAKIT.FIRST.useCase_1" })}</p>
+          </div>
+        </div>
+
+        <div className={styles.itemCard}>
+          <div className={styles.svgWrapper}>
+            <Icon icon="SUAKIT_FEATURE_3" />
+          </div>
+          <div className={styles.cardTitle}>Classification</div>
+          <div className={styles.cardContent}>
+            {intl.formatMessage({ id: "SUAKIT.FIRST.cardContent_2" })}
+            <p className={styles.useCase}>{intl.formatMessage({ id: "SUAKIT.FIRST.useCase_1" })}</p>
+          </div>
+        </div>
+
+        <div className={styles.itemCard}>
+          <div className={styles.svgWrapper}>
+            <Icon icon="SUAKIT_FEATURE_1" />
+          </div>
+          <div className={styles.cardTitle}>Detection</div>
+          <div className={styles.cardContent}>
+            {intl.formatMessage({ id: "SUAKIT.FIRST.cardContent_3" })}
+            <p className={styles.useCase}>{intl.formatMessage({ id: "SUAKIT.FIRST.useCase_1" })}</p>
+          </div>
+        </div>
+      </div>
+      <br/><br/>
       <div className={styles.subTitle}>{intl.formatMessage({ id: "SUAKIT.FIRST.subTitle_1" })}</div>
       <div className={`${styles.itemWrapper} ${styles.methodItemWrapper}`}>
         <div className={styles.itemCard}>
@@ -49,42 +85,6 @@ const FirstSection = ({ intl }) => (
           </div>
           <div className={styles.cardTitle}>One Class Learning</div>
           <div className={styles.cardContent}>{intl.formatMessage({ id: "SUAKIT.FIRST.methodContent_4" })}</div>
-        </div>
-      </div>
-
-      <div className={styles.subTitle}>{intl.formatMessage({ id: "SUAKIT.FIRST.subTitle_2" })}</div>
-      <div className={styles.itemWrapper}>
-        <div className={styles.itemCard}>
-          <div className={styles.svgWrapper}>
-            <Icon icon="SUAKIT_FEATURE_2" />
-          </div>
-          <div className={styles.cardTitle}>Segmentation</div>
-          <div className={styles.cardContent}>
-            {intl.formatMessage({ id: "SUAKIT.FIRST.cardContent_1" })}
-            <p className={styles.useCase}>{intl.formatMessage({ id: "SUAKIT.FIRST.useCase_1" })}</p>
-          </div>
-        </div>
-
-        <div className={styles.itemCard}>
-          <div className={styles.svgWrapper}>
-            <Icon icon="SUAKIT_FEATURE_3" />
-          </div>
-          <div className={styles.cardTitle}>Classification</div>
-          <div className={styles.cardContent}>
-            {intl.formatMessage({ id: "SUAKIT.FIRST.cardContent_2" })}
-            <p className={styles.useCase}>{intl.formatMessage({ id: "SUAKIT.FIRST.useCase_1" })}</p>
-          </div>
-        </div>
-
-        <div className={styles.itemCard}>
-          <div className={styles.svgWrapper}>
-            <Icon icon="SUAKIT_FEATURE_1" />
-          </div>
-          <div className={styles.cardTitle}>Detection</div>
-          <div className={styles.cardContent}>
-            {intl.formatMessage({ id: "SUAKIT.FIRST.cardContent_3" })}
-            <p className={styles.useCase}>{intl.formatMessage({ id: "SUAKIT.FIRST.useCase_1" })}</p>
-          </div>
         </div>
       </div>
     </div>
--- src/components/productPage/suakitPage/secondSection/index.jsx
+++ src/components/productPage/suakitPage/secondSection/index.jsx
@@ -1,33 +1,50 @@
 import React from "react";
 
 import styles from "./secondSection.scss";
+const MAX_EXAMPLE_CNT = 5;
 
-const SecondSection = ({ intl, example }) => (
-  <section className={styles.secondSection}>
-    <div className={styles.innerContainer}>
-      <div className={styles.line} />
-      <div className={styles.subTitle}>{intl.formatMessage({ id: "SUAKIT.SECOND.subTitle" })}</div>
-      <div className={styles.contentWrapper}>
+const SecondSection = ({ intl, example }) => {
+  const page = Math.ceil(example.length / MAX_EXAMPLE_CNT);
+
+  let arr = [];
+  for (let index = 0; index < page; index++) {
+    arr.push(index)
+  }
+  console.log(arr);
+  return (
+    <section className={styles.secondSection}>
+      <div className={styles.innerContainer}>
+        <div className={styles.line} />
+        <div className={styles.subTitle}>{intl.formatMessage({ id: "SUAKIT.SECOND.subTitle" })}</div>
         {
-          example.map(ex => {
-            return(
-              <div className={styles.contentItem} key={ex.id}>
-                <div className={styles.imgWrapper}>
-                  <img src={ex.image} alt="" />
-                </div>
-                <p className={styles.imgCaption}>
-                {intl.locale === "ko" ? ex.name_kr : ex.name_en}
-                </p>
+          arr.map(ar => {
+            const start = ar === 0 ? 0 : ar * MAX_EXAMPLE_CNT;
+            const end = (ar + 1) * MAX_EXAMPLE_CNT;
+            return (
+              <div className={styles.contentWrapper} key={ar}>
+              {
+                example.slice(start, end).map(ex => {
+                  return(
+                    <div className={styles.contentItem} key={ex.id}>
+                      <div className={styles.imgWrapper}>
+                        <img src={ex.image} alt="" />
+                      </div>
+                      <p className={styles.imgCaption}>
+                      {intl.locale === "ko" ? ex.name_kr : ex.name_en}
+                      </p>
+                    </div>
+                  )
+                })
+              }
               </div>
             )
           })
         }
+        <div className={styles.etcContent}>{intl.formatMessage({ id: "SUAKIT.SECOND.etcContent" })}</div>
       </div>
-
-      <div className={styles.etcContent}>{intl.formatMessage({ id: "SUAKIT.SECOND.etcContent" })}</div>
-    </div>
-  </section>
-);
+    </section>
+  )
+};
 
 
 
